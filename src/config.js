1bace73c79c9feb1678e4f4efa8608b75c0ec059
ScapeHelp <scapesonline@hotmail.com>
2026-08-19 22:28:25 +0100

    Delete src directory

--- src/config.js
+++
@@ -1,6 +0,0 @@
-export const siteConfig = {
-  // Replace this value if your permanent Discord invite uses a different slug.
-  discordUrl: 'https://discord.gg/venturerp',
-  serverName: 'Venture Roleplay',
-  tagline: 'Immersive Storytelling & Quality Roleplay',
-}
